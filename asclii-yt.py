1961937bdcecf50ac6fd3a0ea855a28a00d35513
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-10-08 18:24:53 +1000

    Link downloading/error handling

--- asclii-yt.py
+++ asclii-yt.py
@@ -1,5 +1,6 @@
-from sys    import argv
-from pytube import YouTube
+from sys                import argv
+from pytube             import YouTube
+from pytube.exceptions  import AgeRestrictedError, VideoUnavailable
 
 # https://www.youtube.com/watch?v=yhB3BgJyGl8 for testing
 
@@ -7,17 +8,41 @@ from pytube import YouTube
 # Argv
 DIMS_IND        = 2
 GOOD_ARGV       = 3
+LINK_IND        = 1
 
 # Dimensions
 DIMS_DELIM      = "x"
 
 # Error messages
+AGE_RESTRICTED  = "Age restricted videos cannot be downloaded"
 BAD_ARGV        = "Usage: python3 asclii-yt.py link (width)x(height)"
 BAD_DIM_COUNT   = "Two numeric frame dimensions are required"
-#BAD_DIM_VALS    = "Frame dimensions must be numeric"
+BAD_LINK        = "Invalid youtube link provided"
+DOWNLOAD_FAIL   = "Video could not be downloaded"
+NOT_A_LINK      = "Link provided is not a youtube video"
 
 # Exit values
 BAD_ARGS        = 1
+
+
+def download(link):
+    try:
+        youtubeObject = YouTube(link)
+    except:
+        print(NOT_A_LINK)
+    else:
+        try:
+            youtubeObject = youtubeObject.streams.get_highest_resolution()
+        except AgeRestrictedError:
+            print(AGE_RESTRICTED)
+        except VideoUnavailable:
+            print(NOT_A_LINK)
+        else:
+            try:
+                youtubeObject.download()
+            except:
+                print(DOWNLOAD_FAIL)
+
 
 def validate(argv):
     width, height = None, None
@@ -36,6 +61,8 @@ def main():
     width, height = validate(argv)
     if width is None:
         exit(BAD_ARGS)
+
+    download(argv[LINK_IND])
 
     print("Valid :)")
 
